b7c00d9d15ea512c822285e116898158f155fb01
Steve McDonald <steve@SMcDonald-MacBook-Pro.local>
2017-06-28 17:32:10 -0700

    Added login functionality

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
+"use strict"
+
 var express = require('express');
+
 var morgan = require('morgan')
 var app = express();
 var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
 var db = require('./db/db');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var LocalStrategy = require('passport-local').Strategy;
 
 // user model
-var User = require('./model/user')(db)
+var User = require('./model/user');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-app.use(express.static('./public'));
 app.use(morgan('dev'));
+app.use(session({secret: 'blahblah', resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+app.use(express.static('./public'));
 
 passport.serializeUser((user, done) => {
-  done(null, user.id)
+  done(null, user);
 })
 
-passport.deserializeUser((id, done) => {
-  done(err, user)
+passport.deserializeUser((user, done) => {
+  done(null, user);
 })
 
-passport.use(new LocalStrategy(
-    {usernameField: 'username', passwordField: 'password'},
-    function(username, password, done) {
-      User.isValidUser(username, password, done)
+passport.use('login', new LocalStrategy(
+    {usernameField: 'email', passwordField: 'password', passReqToCallback: true, session: true},
+    function(req, username, password, done) {
+
+      var user = User.isValidUser(username, password);
+      user.then(
+          function() {
+            console.log('ok')
+            user.resolve(done(null, user));
+          }
+      ).catch(
+          function() {
+            console.log('fail')
+            user.reject(done(null, false, req.flash('loginFailed', 'No user found.')))
+          }
+      )
+    }
+))
+
+app.get('/login', (req, res) => {
+  res.render('login', {message: req.flash('loginFailed')})
+})
+
+app.post('/login',
+passport.authenticate('login',
+    {
+      successRedirect : '/profile', // redirect to the secure profile section
+      failureRedirect : '/login', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
     }
 ))
 
@@ -32,6 +72,10 @@ passport.use(new LocalStrategy(
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.get('/profile', function(req, res){
+  res.render('profile')
+})
 
 app.get('/signup', function(req, res){
   res.render('signup')
--- model/user.js
+++ model/user.js
@@ -1,22 +1,21 @@
+"use strict"
+
 const bcrypt = require('bcrypt-nodejs');
 const moment = require('moment');
+const db = require('../db/db');
 
-Class User {
-
-  constructor(db) {
-    this.db = db;
-  }
+class User {
 
   createUser(email, password, name) {
     var hash = bcrypt.hashSync(password);
-    return this.db.one({
+    return db.one({
       text: 'INSERT INTO users (name, email, password, date_joined)',
       values: [name, email, hash, moment().format()]
     })
   }
 
   findByEmail(email) {
-    return this.db.one({
+    return db.one({
       text: 'SELECT * FROM users WHERE email = $1',
       values: [email]
     })
@@ -24,7 +23,7 @@ Class User {
 
   isValidUser(email, password) {
     var hash = bcrypt.hashSync(password);
-    return this.db.one({
+    return db.one({
       text: 'SELECT * FROM users WHERE email = $1 and password = $2',
       values: [email, hash]
     })
@@ -32,14 +31,14 @@ Class User {
 
   updateByEmail(old_email, new_email, password) {
     if (new_email) {
-      this.db.one({
+      db.one({
         text: 'UPDATE users SET email = $1 WHERE email = $2',
         values: [new_email, old_email]
       })
     }
     if (password) {
       var hash = bcrypt.hashSync(password);
-      this.db.one({
+      db.one({
         text: 'UPDATE users SET password = $1 WHERE email = $2',
         values: [hash, old_email]
       })
